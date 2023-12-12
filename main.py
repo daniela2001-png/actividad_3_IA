774a279ef565bd68ec1a109d3f22873ba17bd6bc
deividrojas <deivid3221@hotmail.com>
2023-12-12 17:46:14 -0500

    clean the data and plot the correlation

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 # Gets dataframe from the source csv local file
 df = pd.read_csv('./Salary_dataset.csv')
+
+# Checking the quality of the data
+print(df.isnull().values.any())
+print(df.dtypes)
+print(df.info())
+
+# Drop a column that is not relevant for the training
+df = df.drop('Unnamed: 0', axis=1)
+
+# Check the 2 columns that we'll use
+print(df.columns)
+x = df.YearsExperience
+y = df.Salary
+
+# Made a plot to have a preview of the correlation and if is positive or negative
+fig, ax = plt.subplots()
+ax.scatter(x,y)
+plt.show()
+
+
+
